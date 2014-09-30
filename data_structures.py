2754d7562bd4f75439fc4670d3365d9e521ee991
astroScott <s1gustaf@gmail.com>
2014-09-30 10:49:41 -0700

    text utils, removed cmd line stuff

--- data_structures.py
+++ data_structures.py
@@ -1,16 +1,3 @@
-""" 
-command line script to write new values to xml or to dump current vals into 
-text file.
-a typical command will be like:
-    >>> python cmd_line_io.py get file.xml id=foobar id2=foo id3=bar
-this will dump all the data from the listed absorbers to a file 
-`absorberData.txt`.
-
-    >>> python cmd_line_io.py write file.xml id=foobar N=12.5 b=8.3 z=4.0
-will right the fitting parameters back into the xml
-        
-
-"""
 
 import xmlutils
 import data_types
@@ -480,122 +467,6 @@ class ModelDB(object):
 
 
 #some helper functions
-
-
-def read_in(name,return_db=True):
-    """
-    read in a model database from text file
-    Inputs:
-    ------
-    name: filename of model database (default = model_database.txt)
-
-    returns:
-    --------
-    ModelDB instance
-    """
-
-    assert(type(name) is str)
-    try:
-        f = open(name,'r')
-    except:
-        raise Exception(str(name))
-    models = []
-    id=0
-    inp = f.readlines()
-    assert(len(inp)>1)
-    xmlfile = str(inp[0].strip())
-    if ".xml" not in xmlfile:
-        msg = "relevant dude .xml file must be notated as line 1 of %s\ngot %s instead"%(name,xmlfile)
-        print(msg)
-        xmlfile = input("enter relevant file:  ")
-    i=1
-    while i<len(inp):
-        temp = []
-        while i<len(inp):  #yes this looks weird, but is correct.
-            if inp[i].strip()!='\n': 
-                temp.append(inp[i])
-                if 'chi2' in inp[i]: 
-                    i+=1
-                    break
-                else:
-                    i+=1
-        if i>=len(inp):
-            break
-        newmod = parse_single_model(xmlfile, temp, id=str(id))
-        models.append(newmod) 
-        id+=1
-        i+=1
-    if return_db:
-        return ModelDB(name, models=models) 
-    else:
-        return models 
-
-
-
-def parse_abs(xmlfile,data):
-    """
-    parse an individual absorber, either from a list of arguments or a raw string with single absorber
-    """
-    data = re.sub('=\s+','=', data.strip()).split()
-    dct=dict([item.split('=') for item in data])
-    dct["xmlfile"] = xmlfile
-    return data_types.Absorber(**dct)
-
-def parse_single_model(xmlfile, lines, id=None):   
-    """parse a string representation of a single model.
-
-    an example model is:
-    id=HI    N=17.12345 b=12.345678 z=1.234567890
-    id=SiI   N=11.12345 b=12.345678 z=1.234567890
-    id=OI    N=11.12345 b=12.345678 z=1.234567890
-    locked=OI:bLocked HI:zLocked
-    chi2=123.4 pixels=154
-
-    different models are differentiated by empty lines.
-
-    Inputs:
-    -------
-    lines:  a list of a few lines of text
-
-    returns:
-    --------
-    Model instance
-    """
-
-    try:
-        str(xmlfile)
-    except:
-        xmlfile = str(xmlfile.name)
-
-    absorbers = []
-    for line in lines:
-        if line[0:4]=='id':
-            ab = parse_abs(xmlfile, line)
-            absorbers.append(ab)
-        elif line[0:4]=='lock':
-            lst=(line.split('=')[1]).split()
-            dic = dict([item.split(':') for item in lst])
-            for key, val in dic.items():
-                for item in absorbers:
-                    if item.id==key.strip():
-                        setattr(item,val.strip(),True)
-        elif 'chi2' in line:  #this should always be the last line
-            lst=line.split()
-            kw=dict([(item.strip()).split('=') for item in lst])
-            return Model(absorbers=absorbers, id=id, **kw)
-        else: 
-            pass
-    raise Exception("Input error for model database")
-
-def random(xmlfile,itemid,tag,param,val_range,modelid=None):
-    """get a new random setting a parameter to a random value in val_range (tuple or list)"""
-    mod=Model(xmlfile=xmlfile,chi2=0,pixels=0,params=0,id=modelid)
-    old = mod.get(itemid,tag,"N")
-    mod.monte_carlo_set(itemid,tag,param,val_range)
-    new=mod.get(itemid,tag,"N")
-    assert(old!=new)
-    print("model written to %s"%(xmlfile))
-
 def to_bool(string):
     string = string.lower()
     if string=="true":
@@ -608,11 +479,4 @@ def inv_dict(tag, dic=model_classes):
         tmp = {v:k for k, v in dic.items()} 
         return tmp[tag]
 
-def newdb(xmlfile,chi2,pixels,params,dbfile=None,**kwargs):
-    """get a model, append to new database"""
-    model = Model(xmlfile=xmlfile,chi2=chi2,pixels=pixels,params=params)
-    return ModelDB(dbfile,[model],**kwargs)
-
-def getdb(dbfile):
-    return ModelDB.read(dbfile)
 
---
+++ dudeutils.py
@@ -0,0 +1,20 @@
+from data_structures import Model, ModelDB
+
+"""a collection of helper functions"""
+
+def newdb(xmlfile,chi2,pixels,params,dbfile=None,**kwargs):
+    """get a model, append to new database"""
+    model = Model(xmlfile=xmlfile,chi2=chi2,pixels=pixels,params=params)
+    return ModelDB(dbfile,[model],**kwargs)
+
+def getdb(dbfile):
+    return ModelDB.read(dbfile)
+
+def random(xmlfile,itemid,tag,param,val_range,modelid=None):
+    """get a new random setting a parameter to a random value in val_range (tuple or list)"""
+    mod=Model(xmlfile=xmlfile,chi2=0,pixels=0,params=0,id=modelid)
+    old = mod.get(itemid,tag,"N")
+    mod.monte_carlo_set(itemid,tag,param,val_range)
+    new=mod.get(itemid,tag,"N")
+    assert(old!=new)
+    print("model written to %s"%(xmlfile))
