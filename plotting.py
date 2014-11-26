d3cbd553b66eb63633b128b8a73d9947023689a3
astroScott <s1gustaf@gmail.com>
2014-11-26 13:52:25 -0800

    ...

--- plotting.py
+++ plotting.py
@@ -4,10 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 from constraints import Constraint
-
-def z_from_vel(vel, z1):
-    delz =  float(vel)*(1.+float(z1))/c
-    return z1+delz
+from matplotlib import rc
 
 def xy(x_id, y_id, x,y, dbfile, constraints=None, chi2=False):
     if type(dbfile) is str:
@@ -32,7 +29,7 @@ def make_subplot(x,y, style='bo', xname=None,yname=None, ax=None,xlim=None,ylim=
     if xname: ax.set_xlabel(xname)
     if not xlim is None: ax.set_xlim(xlim)
     if not ylim is None: ax.set_ylim(ylim)
-    
+    ax.ticklabel_format(useOffset=False)
     return line
        
 def plotDH(db,constraints=None):
@@ -57,15 +54,13 @@ def plotND_NH(db,constraints=None):
     ND, NH, chi2 = xy("D","H","N","N",db,constraints=constraints)
     zH, zD, _ =xy("D","H","z","z",db,constraints=constraints) 
 
-    DH = [float(ND[i])-float(NH[i]) for i in range(len(NH))]
-    vel = [(float(zD[i])-float(zH[i]))*c/(1.+float(zH[i])) for i in range(len(zH))]
+    ND = [float(ND[i]) for i in range(len(ND))]
+    NH = [float(NH[i]) for i in range(len(NH))]
     chi2 = [float(chi2[i]) for i in range(len(zH))]
-    #chi2 = list(map(float, chi2))
-    assert(len(DH)==len(vel)==len(chi2))
     fig1, (ax1, ax2) = plt.subplots(nrows=2)
     
-    _1 = make_subplot(vel, DH, ax=ax1, yname='D/H',xlim=[-1.2,1.2])
-    _2 = make_subplot(vel, chi2, ax=ax2, xname='D velocity (km/s)', yname='chi2',xlim=[-1.2,1.2])
+    _1 = make_subplot(ND, NH, ax=ax1, yname='NH',xlim=(12.7,13.1))
+    _2 = make_subplot(ND, chi2, ax=ax2, xname='ND', yname='chi2',xlim=(12.7,13.1))
 
     #fig2 = plt.figure()
     #plot(x, np.cos(x))
@@ -134,8 +129,9 @@ def plotchi2():
 
 if __name__ == '__main__':
 
-    constraints = Constraint(**{"H2":{"z":(2.9875,2.9876)},"chi2":(2130.,2142.)})
+    constraints = Constraint(**{"H2":{"z":(2.98755,2.9876)},"chi2":(2130.,2142.)})
 
     db=dudeutils.load_from_db('2014-11-24db.xml')
     db.trim(constraints)
     plotDH(db)#,constraints={"H2":{"z":(2.9875,2.98765)}})
+    plotND_NH(db)
